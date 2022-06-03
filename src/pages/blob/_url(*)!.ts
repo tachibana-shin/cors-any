04398a5fd5284446e03a22e73d2b8568ac1c0d4e
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-06-03 12:16:52 +0000

    fix lint, add decoded

--- src/pages/blob/_url(*)!.ts
+++ src/pages/blob/_url(*)!.ts
@@ -1,10 +1,11 @@
-import { page } from "epact"
 import axios from "axios"
+import { page } from "epact"
 
 function isUrlHttp(str: string): boolean {
   return !!/https?:\/{2}/.test(str)
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function parseJSON(json?: string): any | null {
   if (!json) return null
 
@@ -17,11 +18,11 @@ function parseJSON(json?: string): any | null {
 
 export default page({
   async get(req, res) {
-    const url = req.params[0]
-    const customHeaders = parseJSON(req.query["cs-headers"])
-    const inheritHeaders = parseJSON(req.query["cs-inherit-headers"])
+    const url = decodeURIComponent(req.params[0])
+    const customHeaders = parseJSON(req.query["cs-headers"] as string)
+    const inheritHeaders = parseJSON(req.query["cs-inherit-headers"] as string)
     const appendResponseHeaders = parseJSON(
-      req.query["cs-append-response-headers"]
+      req.query["cs-append-response-headers"] as string
     )
 
     if (!isUrlHttp(url)) {
@@ -32,28 +33,31 @@ export default page({
     const requestQueries = Object.fromEntries(
       Object.entries(req.query).filter(([name]) => !name.startsWith("cs-"))
     )
+    // eslint-disable-next-line no-undef
     const requestHeaders = new Headers(
-      inheritHeaders === false ? undefined : req.headers
+      inheritHeaders === false
+        ? undefined
+        : (req.headers as Record<string, string>)
     )
 
     if (customHeaders) {
-      for (const [name, value] of Object.entries(customHeaders)) {
-        requestHeaders.append(name, value)
-      }
+      for (const [name, value] of Object.entries(customHeaders))
+        requestHeaders.append(name, value as string)
     }
 
     const response = await axios.get(url, {
-      query: requestQueries,
+      params: requestQueries,
       headers: customHeaders,
       responseType: "arraybuffer"
     })
 
     // set headers
-    if (appendResponseHeaders !== false)
+    if (appendResponseHeaders !== false) {
       for (const [name, value] of Object.entries(response.headers)) {
         if (name.toLowerCase() === "host") continue
         res.append(name, value)
       }
+    }
 
     res.send(Buffer.from(response.data))
   }
